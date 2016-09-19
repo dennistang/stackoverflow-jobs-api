ef96521b777bb2a95f2dbe3805d6dfbf6fd7cbc2
Dennis Tang <dennis@dennistang.net>
2016-09-18 23:45:21 -0700

    feat(jobs): added jobs endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var package = require('../package.json');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.setHeader('Content-Type', 'application/json');
+    
+  res.send({
+    application: 'stackoverflow-jobs-api', 
+    version: package.version
+  });
 });
 
 module.exports = router;
---
+++ routes/jobs.js
@@ -0,0 +1,65 @@
+var express = require('express');
+var router = express.Router();
+var x = require('x-ray')({
+  filters: {
+    trim: function (value) {
+      return typeof value === 'string' ? value.trim() : value
+    },
+    reverse: function (value) {
+      return typeof value === 'string' ? value.split('').reverse().join('') : value
+    },
+    slice: function (value, start , end) {
+      return typeof value === 'string' ? value.slice(start, end) : value
+    },
+    boolean: function (value) {
+      return value === '' ? false : true;
+    }
+  }
+});
+
+serialize = function(obj) {
+  var queryString = [];
+
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      queryString.push(encodeURIComponent(key) + '=' + encodeURIComponent(obj[key]));
+    }
+  }
+
+  return queryString.join('&');
+}
+
+/**
+ * Crawls StackOverflow Jobs page based on query parameters provided.
+ * s&offersvisasponsorship=true&pg=2
+ * @param {string} location - Location, e.g., Berlin, Germany, Pasadena
+ * @param {number} range - Location in distanceUnits, e.g., 20, 50, 100
+ * @param {string} distanceUnits - Unit of distance, e.g., Miles, Kilometers
+ * @param {boolean} allowsremote - Remote filter, defaults to false
+ * @param {boolean} offersrelocation - Relocation filter, defaults to false
+ * @param {boolean} offersvisasponsorship - Visa Sponsorship filter, defaults to false
+ * @param {number} pg - page offset, e.g., 1, 2, 3, ... n
+ */
+router.get('/', function(req, res, next) {
+
+  let url = 'http://stackoverflow.com/jobs?' + serialize(req.query);
+
+  var stream = x(url, 'div.-job', [{
+    id: 'span.fav-toggle@data-jobid',
+    title: '.-title h1 | trim',
+    salary: '.-title .salary | trim',
+    employer: '.employer | trim',
+    permalink: 'a.job-link@href',
+    categories: x('div.tags', ['a']),
+    description: '.description | trim',
+    location: '.metadata .location | trim',
+    visa: '.metadata .visa | boolean',
+    relocation: '.metadata .relocation | boolean',
+    remote: '.metadata .remote | boolean',
+    date: '.posted | trim'
+  }]).stream();
+
+  stream.pipe(res);
+});
+
+module.exports = router;
